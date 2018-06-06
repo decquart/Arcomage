9c13fe02f8153e04240c2a9c1e68d589181eafc8
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-06 22:20:51 +0300

    Added entities

---
+++ Arcomage/Entities/Deck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcomage.Entities
+{
+    class Deck
+    {
+        public ICollection<Card> Cards { get; set; }
+        public Deck()
+        {
+            Cards = new List<Card>();
+        }
+    }
+}
