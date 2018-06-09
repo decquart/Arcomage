aad40a007d2b6c09765837f55534b3a24af4e7b4
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-10 00:48:31 +0300

    Added class which is responsible for deck initialization

---
+++ Arcomage/Services/DeckInitializer.cs
@@ -0,0 +1,19 @@
+﻿using Arcomage.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcomage.Services
+{
+    class DeckInitializer
+    {
+        private Deck deck = new Deck();
+        public Deck Set()
+        {
+            deck.Cards = CardSerializator.GetCardsFromJson();
+            return deck;
+        }
+    }
+}
