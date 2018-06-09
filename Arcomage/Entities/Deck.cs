1ec99462c654f1d38f06548253b66aeecde34fa2
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-09 20:12:23 +0300

    Added some methods to class

--- Arcomage/Entities/Deck.cs
+++ Arcomage/Entities/Deck.cs
@@ -8,10 +8,54 @@ namespace Arcomage.Entities
 {
     public class Deck
     {
-        public ICollection<Card> Cards { get; set; }
+        public List<Card> Cards { get; set; }
+        private static Random rng = new Random();
+
         public Deck()
         {
             Cards = new List<Card>();
+        }
+
+        /*
+         * This method'll call only once, when game'll start
+         */
+        public void Deal(Players players)
+        {
+            //amount of cards, which player'll be have, should to change hardcode value!!!
+            var length = 3; 
+            for (int i = 0; i < length; i++)
+            {
+                GiveCard(players.FirstPlayer);
+                GiveCard(players.SecondPlayer);
+            }
+
+        }
+
+        public void GiveCard(User player)
+        {
+            var card = Cards.FirstOrDefault();
+            if(card != null)
+            {
+                player.Hand.Add(card);
+                Cards.Remove(card);
+            }
+        }
+
+
+        /*
+         * This method mix cards in deck
+         */
+        public void Shuffle()
+        {
+            int n = Cards.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                Card value = Cards[k];
+                Cards[k] = Cards[n];
+                Cards[n] = value;
+            }
         }
     }
 }
