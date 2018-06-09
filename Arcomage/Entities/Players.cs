037c7106dc24707253b7ce3f6c9c2ba7f24a97ec
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-10 00:47:02 +0300

    Added player class

---
+++ Arcomage/Entities/Players.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcomage.Entities
+{
+    public class Players
+    {
+        public User FirstPlayer { get; set; }
+        public User SecondPlayer { get; set; }
+    }
+}
