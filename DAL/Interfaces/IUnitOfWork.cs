943adc057bdebf1eee66930b2e2a6ff5caf95cc9
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-13 23:57:09 +0300

    Added repository and uow interface

---
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Entities;
+using DAL.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    interface IUnitOfWork : IDisposable
+    {
+        IRepository<HighScore, int> Messages { get; }
+        UserManager Users { get; }
+        Task SaveAsync();
+        void Save();
+    }
+}
