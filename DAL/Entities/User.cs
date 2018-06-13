7164d278aac3d9069e4498f501f1c477325d508a
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-13 23:57:44 +0300

    added entities to DAL

---
+++ DAL/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        [MaxLength(25)]
+        public string Name { get; set; }
+
+        public  HighScore HighScore { get; set; }
+    }
+}
