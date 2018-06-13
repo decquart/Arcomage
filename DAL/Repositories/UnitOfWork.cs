5798907e1f9f6e3b426d3f951ce9e97eead4321b
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-13 23:55:54 +0300

    Created repositories and uow class

---
+++ DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,87 @@
+﻿using Arcomage.Context;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        private ApplicationContext db;
+
+        private UserManager userManager;
+        private HighScoreRepository highScore;
+       
+
+        public UserManager Users
+        {
+            get
+            {
+                if (userManager == null)
+                    userManager = new UserManager(db)
+                    {
+                        UserValidator = new UserValidator<User>(userManager)
+                        {
+                            AllowOnlyAlphanumericUserNames = false,
+                            RequireUniqueEmail = true
+                        },
+                        PasswordValidator = new PasswordValidator
+                        {
+                            RequiredLength = 6,
+                            RequireNonLetterOrDigit = false,
+                            RequireDigit = false,
+                            RequireLowercase = true,
+                            RequireUppercase = false,
+                        }
+                    };
+                return userManager;
+            }
+        }
+
+        public IRepository<HighScore, int> Messages
+        {
+            get
+            {
+                if (highScore == null)
+                    highScore = new HighScoreRepository(db);
+                return highScore;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await db.SaveChangesAsync();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                    db.Dispose();
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
---
+++ DAL/Repositories/UserManager.cs
@@ -0,0 +1,58 @@
+﻿using Arcomage.Context;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class UserManager : UserManager<User>, IRepository <User, string> 
+    {
+
+        private ApplicationContext _db;
+
+        public UserManager(ApplicationContext db)
+            :base(new UserStore<User>(db))
+        {
+            _db = db;
+        }
+
+        public void Create(User user)
+        {
+            _db.Users.Add(user);
+        }
+
+        public void Delete(string id)
+        {
+            var user = _db.Users.Find(id);
+            if (user != null)
+                _db.Users.Remove(user);
+        }
+
+        public IEnumerable<User> Find(Func<User, bool> predicate)
+        {
+            return _db.Users.Where(predicate);
+        }
+
+        public User Get(string id)
+        {
+            return _db.Users.Find(id);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _db.Users.ToList();
+        }
+
+        public void Update(User user)
+        {
+            _db.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
