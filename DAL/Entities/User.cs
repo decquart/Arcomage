8ce3ca3da2599c3826c6a948886a141462947d9b
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-14 22:31:32 +0300

    added GenerateUserIdentity method

--- DAL/Entities/User.cs
+++ DAL/Entities/User.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,5 +17,11 @@ namespace DAL.Entities
         public string Name { get; set; }
 
         public  HighScore HighScore { get; set; }
+
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager, string authenticationType)
+        {
+            return await manager.CreateIdentityAsync(this, authenticationType);
+        }
     }
 }
