42ae8b5e5c67542defd2b2ce98e97059dc12cee3
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-17 21:20:51 +0300

    created account controller

---
+++ Web/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Arcomage.Entities;
+using Arcomage.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers 
+{
+    [Produces("application/json")]
+    [Route("api/Account")]
+    public class AccountController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public AccountController(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        //public async Task<IActionResult> Register(RegisterModel model)
+        //{
+        //    return Ok();
+        //}
+
+        [Route("")]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var usersDTO = _userService.GetUsers();
+                if (usersDTO.ToList().Count == 0)
+                    throw new Exception("Users not found");
+                var users = _mapper.Map<IEnumerable<UserDTO>, List<UserModel>>(usersDTO);
+                return Json(users);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+    }
+}
