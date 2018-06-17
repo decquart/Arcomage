05acdb0da796d7e2f379ee1bfff685a5d0d07591
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-17 23:18:22 +0300

    Created services interfaces

---
+++ Arcomage/Interfaces/IDeckService.cs
@@ -0,0 +1,17 @@
+﻿using Arcomage.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcomage.Interfaces
+{
+    public interface IDeckService
+    {
+        void Shuffle();
+        void GiveCardToPlayer(string userId);
+        void PushCard(Card card);
+        void Deal(string firstUserId, string secondUserId);
+    }
+}
---
+++ Arcomage/Interfaces/IGameService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcomage.Interfaces
+{
+    public interface IGameService
+    {
+        void PlayCard(string userId, string cardName);
+        void DiscardCard(string userId, string cardName);
+        void SetResultsToScore(string winId, string losId);
+        string CheckIfGameIsOver(string id, string oppId);
+    }
+}
