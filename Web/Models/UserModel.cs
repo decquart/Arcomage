310622f29585ea93af04187fa40b58bc1ab8f3cb
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-17 21:20:25 +0300

    Added models to the project

---
+++ Web/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+
+        public int Victories { get; set; }
+        public int Defeats { get; set; }
+    }
+}
