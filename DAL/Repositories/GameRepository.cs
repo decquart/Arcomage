124d6dafe193616f7575f771822d7486642e4254
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-21 23:30:44 +0300

    created game and score repository

---
+++ DAL/Repositories/GameRepository.cs
@@ -0,0 +1,53 @@
+﻿using DAL.Context;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL.Repositories
+{
+    class GameRepository : IRepository<Game>
+    {
+        private readonly ApplicationContext _db;
+
+        public GameRepository(ApplicationContext db)
+        {
+            _db = db;
+        }
+
+        public void Create(Game game)
+        {
+            _db.Games.Add(game);
+        }
+
+        public void Delete(int id)
+        {
+            var game = _db.Games.Find(id);
+            if (game != null)
+                _db.Games.Remove(game);
+        }
+
+        public IEnumerable<Game> Find(Func<Game, bool> predicate)
+        {
+            return _db.Games.Where(predicate);
+        }
+
+        public Game Get(int id)
+        {
+            return _db.Games.Find(id);
+        }
+
+        public IEnumerable<Game> GetAll()
+        {
+            return _db.Games.ToList();
+        }
+
+        public void Update(Game game)
+        {
+            _db.Entry(game).State = EntityState.Modified;
+        }
+    }
+}
---
+++ DAL/Repositories/ScoreRepository.cs
@@ -0,0 +1,53 @@
+﻿using DAL.Context;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL.Repositories
+{
+    class ScoreRepository : IRepository<Score>
+    {
+        private readonly ApplicationContext _db;
+
+        public ScoreRepository(ApplicationContext db)
+        {
+            _db = db;
+        }
+
+        public void Create(Score score)
+        {
+            _db.Scores.Add(score);
+        }
+
+        public void Delete(int id)
+        {
+            var score = _db.Scores.Find(id);
+            if (score != null)
+                _db.Scores.Remove(score);
+        }
+
+        public IEnumerable<Score> Find(Func<Score, bool> predicate)
+        {
+            return _db.Scores.Where(predicate);
+        }
+
+        public Score Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Score> GetAll()
+        {
+            return _db.Scores.ToList();
+        }
+
+        public void Update(Score score)
+        {
+            _db.Entry(score).State = EntityState.Modified;
+        }
+    }
+}
