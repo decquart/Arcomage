b7f44a41d5e9a9d7ec5d17d0198536e06803568b
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-21 23:38:37 +0300

    created user data transfer object to specific game

---
+++ Arcomage/DTO/ArcomageUserDTO.cs
@@ -0,0 +1,17 @@
+﻿using Arcomage.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arcomage.DTO
+{
+    public class ArcomageUserDTO : UserDTO
+    {
+        public Castle Castle { get; set; }
+        public List<Card> Hand;
+        public ArcomageUserDTO()
+        {
+            Hand = new List<Card>();
+        }
+    }
+}
