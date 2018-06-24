22d226fc9c421455839a7b73ed89051395af947c
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-25 00:36:15 +0300

    created an interface to score service

---
+++ BLL/Interfaces/IScoreService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Interfaces
+{
+    public interface IScoreService
+    {
+        void AddScore(int userId, int gameId, int value);
+
+        
+    }
+}
