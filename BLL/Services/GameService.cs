02f4652cda26dd07ff01735bac7048e3b7a97957
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-24 20:17:45 +0300

    created game service and dto

---
+++ BLL/Services/GameService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.Interfaces;
+using DAL.Entities;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Services
+{
+    public class GameService : IGameService
+    {
+        IUnitOfWork _db;
+        IMapper _mapper;
+
+        public GameService(IUnitOfWork db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public GameDto Get(int gameId)
+        {
+            var game = _db.Games.Get(gameId);
+            if (game == null)
+                throw new Exception($"Game {gameId} not found");
+
+            return _mapper.Map<Game, GameDto>(game);
+        }
+
+        public IEnumerable<GameDto> GetGameList()
+        {
+            var games = _db.Games.GetAll();
+            return _mapper.Map<IEnumerable<Game>, List<GameDto>>(games);
+        }
+    }
+}
