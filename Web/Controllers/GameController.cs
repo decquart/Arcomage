c7d93dced37f7dfa3eceb83eb887bce8b534f7e8
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-25 00:36:35 +0300

    created game controller

---
+++ Web/Controllers/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.DTO;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Controllers
+{
+    [Route("api/Game")]
+    public class GameController : ControllerBase
+    {
+        private readonly IGameService _gameService;
+
+        public GameController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetAll()
+        {
+            var games = _gameService.GetGameList();
+
+            if (games.Count().Equals(0))
+                return NotFound();
+
+            return Ok(games);
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create([FromBody] GameDto game)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _gameService.CreateGame(game);
+
+            return Ok();
+        }
+    }
+}
