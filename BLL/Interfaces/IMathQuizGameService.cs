5909622cad1e5de59e44ea4ece70e59207b4e04a
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-27 00:38:09 +0300

    created the interface for new game

---
+++ BLL/Interfaces/IMathQuizGameService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Interfaces
+{
+    public interface IMathQuizGameService
+    {
+        string GenerateQuestion(int firstNumber, int secondNumber);
+        int GetrandomNumber();
+        bool CheckAnswer(int correctAnswer, int userAnswer);
+
+    }
+}
