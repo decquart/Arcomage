6855388b68d0312d8c190566687900881d570b33
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-06-28 18:42:48 +0300

    migrate to web api

---
+++ WebApi/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BLL.DTO;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        //private readonly UserManager<User> _userManager;
+        //private readonly SignInManager<User> _signInManager;
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public AccountController(IMapper mapper, IUserService userService)
+        {
+            _mapper = mapper;
+            _userService = userService;
+        }
+
+        //public async Task<IActionResult> Register(RegisterModel model)
+        //{
+        //    return Ok();
+        //}
+
+
+
+        [Route("")]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var users = _userService.GetUsers();
+                if (users.ToList().Count == 0)
+                    throw new Exception("Users not found");
+                //var users = _mapper.Map<IEnumerable<UserDTO>, List<UserModel>>(usersDTO);
+                return Ok(users);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Register([FromBody] RegisterModel user)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            var usr = new UserDTO { Email = user.Email, Password = user.Password, Name = user.Email };
+            var result = _userService.Register(usr).GetAwaiter().GetResult();
+
+            if (result == null)
+                return BadRequest();
+
+            return Ok();
+        }
+    }
+}
---
+++ WebApi/Controllers/GameController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.DTO;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private readonly IGameService _gameService;
+
+        public GameController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Get([FromRoute] long gameId)
+        {
+            var game = _gameService.Get((int)gameId);
+
+            if (game == null)
+                return NotFound();
+
+            return Ok(game);
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetAll()
+        {
+            var games = _gameService.GetGameList();
+
+            if (games.Count().Equals(0))
+                return NotFound();
+
+            return Ok(games);
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create([FromBody] GameDto game)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _gameService.CreateGame(game);
+
+            return Ok();
+        }
+    }
+}
