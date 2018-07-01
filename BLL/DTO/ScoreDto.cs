f751824c800c2508fbc6315d6700741607877521
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-02 00:01:20 +0300

    created score dto

---
+++ BLL/DTO/ScoreDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.DTO
+{
+    public class ScoreDto
+    {
+        public int Id{ get; set; }
+        public int Value { get; set; }
+    }
+}
