bca0fb43dedb85190319f45b2dadde36d75385f1
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-02 14:39:30 +0300

    created score controller, added api

---
+++ WebApi/Controllers/ScoreController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScoreController : Controller
+    {
+        private readonly IScoreService _scoreService;
+
+        public ScoreController(IScoreService scoreService)
+        {
+            _scoreService = scoreService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public  IActionResult Create([FromBody] ScoreModel scoreModel)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest();
+
+                _scoreService.AddScore(scoreModel.UserId, scoreModel.GameId, scoreModel.Value);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+           
+            return Ok();
+        }
+    }
+} 
