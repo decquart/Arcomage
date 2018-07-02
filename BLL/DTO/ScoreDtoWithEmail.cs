c4a02b6d68846fbec698683ecb9409997f98740c
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-02 14:32:44 +0300

    created new model to store emails and user scores

---
+++ BLL/DTO/ScoreDtoWithEmail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.DTO
+{
+    public class ScoreDtoWithEmail
+    {
+        public int Id { get; set; }
+        public int Value { get; set; }
+        public string Email { get; set; }
+    }
+}
