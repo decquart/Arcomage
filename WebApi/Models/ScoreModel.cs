f06c512d8afa1be2dfad1b10e6d4e0a1de316db2
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-02 11:59:48 +0300

    created helper model

---
+++ WebApi/Models/ScoreModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class ScoreModel
+    {
+        public int Value { get; set; }
+        public int GameId { get; set; }
+        public string  UserId { get; set; }
+    }
+}
