a64a6bbfdf3511959fc7348f6e72b892abec2ed8
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-05 16:12:39 +0300

    added deck interface

---
+++ BLL/Interfaces/IDeck.cs
@@ -0,0 +1,12 @@
+﻿using BLL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Interfaces
+{
+    public interface IDeck
+    {
+        List<Card> Cards { get; set; }
+    }
+}
