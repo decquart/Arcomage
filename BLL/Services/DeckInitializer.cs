3d189261cbb8d569987e351fb15c7f6290ab3dff
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-05 20:30:19 +0300

    remove unnecessary logic

--- BLL/Services/DeckInitializer.cs
+++ BLL/Services/DeckInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using BLL.Entities;
+using BLL.Helper;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,63 +8,19 @@ namespace BLL.Services
 {
     public class DeckInitializer
     {
-        private Deck _deck = new Deck();
-        private CardBehavior cardBehavior;
+        private Deck _deck;
 
-        public DeckInitializer(Game game)
+        public DeckInitializer()
         {
-            cardBehavior = new CardBehavior(game);
+            _deck = new Deck();
         }
         public Deck Set()
         {
             var cards = new CardInitializer().GetCardsFromJson();
-            DelegateInitialize(cards);
             _deck.Cards = cards;
             return _deck;
         }
 
-        public void DelegateInitialize(List<Card> cardList)
-        {
-            var concertedCardDictionary = ConcertedCardDictionary();
-
-            // it'll be explain later )
-            foreach (var card in cardList)
-            {
-                foreach (var keyVal in concertedCardDictionary)
-                {
-                    if (card.Method == keyVal.Key)
-                        card.Del += keyVal.Value;
-                }
-
-
-                //foreach (var methodstring in jsonItem)
-                //{
-                //    counter++;
-                //    foreach (var method in concertedcarddictionary)
-                //    {
-                //        if (methodstring == method.key)
-                //            console.writeline("ss");
-                //        //todo 
-                //    }
-                //}
-            }
-        }
-
-
-        private Dictionary<string, PlayFunction> ConcertedCardDictionary()
-        {
-            var dict = new Dictionary<string, PlayFunction>();
-            dict.Add("Damage", cardBehavior.Damage);
-            dict.Add("AddWall", cardBehavior.AddWall);
-            dict.Add("AddMagic", cardBehavior.AddMagic);
-            dict.Add("AddDungeon", cardBehavior.AddDungeon);
-            dict.Add("AddQuarry", cardBehavior.AddQuarry);
-            dict.Add("AddBricks", cardBehavior.AddBricks);
-            dict.Add("AddGems", cardBehavior.AddGems);
-            dict.Add("AddRecruits", cardBehavior.AddRecruits);
-            dict.Add("AddCastle", cardBehavior.AddCastle);
-
-            return dict;
-        }
+        
     }
 }
