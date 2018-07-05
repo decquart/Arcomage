565e5d5d8204f249f072a3c8c4b201269d78b900
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-05 20:14:52 +0300

    added new models

---
+++ WebApi/Models/StartGameModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class StartGameModel
+    {
+        public string FirstPlayerId { get; set; }
+        public string SecondPlayerId { get; set; }
+    }
+}
