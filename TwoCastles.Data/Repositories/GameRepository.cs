14849d533941a2e90128dddccd9d47ad0c051091
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-07 01:06:06 +0300

    created game repository

---
+++ TwoCastles.Data/Repositories/GameRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwoCastles.Data.Interfaces;
+using TwoCastles.Entities;
+
+namespace TwoCastles.Data.Repositories
+{
+    public class GameRepository : IGameRepository
+    {
+        public Game GetGame()
+        {
+            return InMemoryStore.GetGame();
+        }
+
+        public bool Update(Game game)
+        {
+            return InMemoryStore.Update(game);
+        }
+    }
+}
