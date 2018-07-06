204f5a0a530b7ebbc53997b23558ae61a384b77e
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-06 22:57:54 +0300

    Craete separate solution for game

---
+++ TwoCastles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwoCastles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
