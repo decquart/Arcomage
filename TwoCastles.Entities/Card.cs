161ea665290d5673f88d9d89d11b572cf2f25a6f
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-06 23:40:27 +0300

    created entities project, added entities classes

---
+++ TwoCastles.Entities/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TwoCastles.Entities
+{
+    [Serializable]
+    public class Card
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int BrickCost { get; set; }
+        public int GemCost { get; set; }
+        public int RecruitCost { get; set; }
+        public string Colour { get; set; }
+        public bool IsPlayAgain { get; set; }
+        public string Method { get; set; }
+        public int Argument { get; set; }
+    }
+}
---
+++ TwoCastles.Entities/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TwoCastles.Entities
+{
+    public abstract class Player
+    {
+        public Castle Castle { get; set; }
+        public List<Card> Hand;
+
+        protected Player()
+        {
+            Hand = new List<Card>();
+        }
+    }
+}
