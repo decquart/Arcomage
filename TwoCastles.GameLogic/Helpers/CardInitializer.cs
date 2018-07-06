9f30e448ab1fe958ae119f5f30afac9c73c9569d
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-07 02:30:44 +0300

    added helper method

---
+++ TwoCastles.GameLogic/Helpers/CardInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using TwoCastles.Entities;
+
+namespace TwoCastles.GameLogic.Helpers
+{
+    public class CardInitializer
+    {
+        public List<Card> GetCardsFromJson()
+        {
+            var cards = JsonConvert.DeserializeObject<List<Card>>(File.ReadAllText("Configs/Cards.json"));
+
+            return cards;
+        }
+    }
+}
