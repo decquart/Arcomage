d86b43e33210b531aa627151c423f8c5615942c0
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-06 23:59:59 +0300

    created new project with game logic, added some logic to service

---
+++ TwoCastles.GameLogic/Services/CardService.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwoCastles.Entities;
+
+namespace TwoCastles.GameLogic.Services
+{
+    public class CardService
+    {
+        public void Play(Card card, Player currentPlayer, Player enemyPlayer)
+        {
+            if (currentPlayer == null || enemyPlayer == null)
+                throw new ArgumentException($"Players is not valid");
+
+            PayCardPrice(card, currentPlayer);
+            ApplyCardAction(card, currentPlayer, enemyPlayer);
+        }
+
+        void PayCardPrice(Card card, Player currentPlayer)
+        {
+            bool isEnoughRes = isEnoughResources(card, currentPlayer);
+            if (!isEnoughRes)
+                throw new Exception($"Player {currentPlayer} doesn't have enought resources to apply {card.Name}");
+            if (card.BrickCost > 0)
+                currentPlayer.Castle.Bricks -= card.BrickCost;
+            if (card.GemCost > 0)
+                currentPlayer.Castle.Gems -= card.GemCost;
+            if (card.RecruitCost > 0)
+                currentPlayer.Castle.Gems -= card.RecruitCost;
+        }
+
+        void ApplyCardAction(Card card, Player currentPlayer, Player oppPlayer)
+        {
+            switch (card.Method)
+            {
+                case "AddQuarry":
+                    AddQuarry(card.Argument, currentPlayer);
+                    break;
+                case "AddMagic":
+                    AddMagic(card.Argument, currentPlayer);
+                    break;
+                case "AddDungeon":
+                    AddDungeon(card.Argument, currentPlayer);
+                    break;
+                case "AddWall":
+                    AddWall(card.Argument, currentPlayer);
+                    break;
+                case "AddCastle":
+                    AddCastle(card.Argument, currentPlayer);
+                    break;
+                case "Damage":
+                    Damage(card.Argument, currentPlayer);
+                    break;
+                case "AddBricks":
+                    AddBricks(card.Argument, currentPlayer);
+                    break;
+                case "AddGems":
+                    AddGems(card.Argument, currentPlayer);
+                    break;
+                case "AddRecruits":
+                    AddRecruits(card.Argument, currentPlayer);
+                    break;
+                default:
+                    Discard();
+                    break;
+            }
+        }
+
+
+        bool isEnoughResources(Card card, Player currentPlayer)
+        {
+            if (card.BrickCost > currentPlayer.Castle.Bricks ||
+                card.GemCost > currentPlayer.Castle.Gems ||
+                card.RecruitCost > currentPlayer.Castle.Recruits)
+                return false;
+            return true;
+        }
+
+
+
+        void Damage(int amount, Player user)
+        {
+            if (user.Castle.Wall >= amount)
+                user.Castle.Wall -= amount;
+            else
+            {
+                var oldWallHeight = user.Castle.Wall;
+                var oldCastleHeight = user.Castle.Height;
+                if (user.Castle.Wall > 0)
+                    user.Castle.Wall = 0;
+
+                user.Castle.Height -= (amount - oldWallHeight);
+            }
+        }
+
+        void AddWall(int amount, Player user)
+        {
+            user.Castle.Wall += amount;
+        }
+
+        void AddMagic(int amount, Player user)
+        {
+            user.Castle.Magic += amount;
+        }
+
+        void AddDungeon(int amount, Player user)
+        {
+            user.Castle.Dungeon += amount;
+        }
+
+        void AddQuarry(int amount, Player user)
+        {
+            user.Castle.Quarry += amount;
+        }
+
+        void AddBricks(int amount, Player user)
+        {
+            user.Castle.Bricks += amount;
+        }
+
+        void AddGems(int amount, Player user)
+        {
+            user.Castle.Gems += amount;
+        }
+
+        void AddRecruits(int amount, Player user)
+        {
+            user.Castle.Recruits += amount;
+        }
+
+        void AddCastle(int amount, Player user)
+        {
+            user.Castle.Height += amount;
+        }
+
+        void Discard()
+        {
+            return;
+        }
+    }
+}
