88e57e5c78bdd19a5d376b6ad17083f66aac5207
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-07 00:55:55 +0300

    created inMemory store, added 2 methods into

---
+++ TwoCastles.Data/Repositories/InMemoryStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwoCastles.Data.Interfaces;
+using TwoCastles.Entities;
+
+namespace TwoCastles.Data.Repositories
+{
+    internal static class InMemoryStore
+    {
+        private static Game Game = new Game();
+
+        public static Game GetGame()
+        {
+            return Game;
+        }
+
+        public static bool Update(Game game)
+        {
+            if (game == null)
+                throw new ArgumentException("Update isn't possible, invalid game argument");
+            Game = game;
+            return true;
+        }
+    }
+}
+
+
+
