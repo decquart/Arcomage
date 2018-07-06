b56893961fbedc1c1ae71b10a6859bea9db0ab9f
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-07 01:43:23 +0300

    implemented wrapper to data access

---
+++ TwoCastles.Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoCastles.Data.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IGameRepository Game { get; }
+    }
+}
---
+++ TwoCastles.Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwoCastles.Data.Interfaces;
+using TwoCastles.Entities;
+
+namespace TwoCastles.Data.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private GameRepository gameRepository;
+
+        public IGameRepository Game
+        {
+            get
+            {
+                if (gameRepository == null)
+
+                    if (gameRepository == null)
+                        gameRepository = new GameRepository();
+                return gameRepository;
+            }
+        }
+    }
+}
