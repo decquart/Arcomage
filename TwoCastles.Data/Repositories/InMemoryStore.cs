711a741960b1754761afe14667ae960da657d021
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-07 12:26:54 +0300

    displacement game initialization to data access layer

--- TwoCastles.Data/Repositories/InMemoryStore.cs
+++ TwoCastles.Data/Repositories/InMemoryStore.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TwoCastles.Data.Helper;
 using TwoCastles.Data.Interfaces;
 using TwoCastles.Entities;
 
@@ -15,12 +16,43 @@ namespace TwoCastles.Data.Repositories
             return Game;
         }
 
+        public static Game GetNewGame()
+        {
+            Init();
+            return Game;
+        }
+
         public static bool Update(Game game)
         {
             if (game == null)
                 throw new ArgumentException("Update isn't possible, invalid game argument");
             Game = game;
             return true;
+        }
+
+        static Game Init()
+        {
+            Game.CurrentDeck = new Deck { Cards = new JsonParser().GetCardsFromJson() };
+            Game.FirstPlayer = new Player();
+            Game.SecondPlayer = new Player();
+            CastleInit(Game.FirstPlayer.Castle = new Castle());
+            CastleInit(Game.SecondPlayer.Castle = new Castle());
+
+            return Game;
+        }
+
+        static void CastleInit(Castle castle)
+        {
+            castle.Height = 20;
+            castle.Wall = 5;
+
+            castle.Bricks = 5;
+            castle.Gems = 5;
+            castle.Recruits = 5;
+
+            castle.Quarry = 2;
+            castle.Magic = 2;
+            castle.Dungeon = 2;
         }
     }
 }
