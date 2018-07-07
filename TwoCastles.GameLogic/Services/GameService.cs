dde9920d70c4456077f0a0be2ff1d75e1fffb00f
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-07 03:17:20 +0300

    created game service, added method which will normalize data inside the castle after each player action

---
+++ TwoCastles.GameLogic/Services/GameService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwoCastles.Entities;
+
+namespace TwoCastles.GameLogic.Services
+{
+    public class GameService
+    {
+        private readonly Game _game;
+        public GameService(Game game)
+        {
+            _game = game;
+        }
+
+        public void NormalizeCastle(Castle castle)
+        {
+            if (castle.Quarry < 1) castle.Quarry = 1;
+            if (castle.Magic < 1) castle.Magic = 1;
+            if (castle.Dungeon < 1) castle.Dungeon = 1;
+
+            if (castle.Bricks < 0) castle.Bricks = 0;
+            if (castle.Gems < 0) castle.Gems = 0;
+            if (castle.Recruits < 0) castle.Recruits = 0;
+
+            if (castle.Wall < 0)
+                castle.Wall = 0;
+        }
+    }
+}
