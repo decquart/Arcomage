1531e4e4e5e060262d6a151cefe4f6a1aeee3416
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-08 22:06:54 +0300

    added new entity for register and login

---
+++ BLL/DTO/UserDtoForRegister.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.DTO
+{
+    public class UserDtoForRegister
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
