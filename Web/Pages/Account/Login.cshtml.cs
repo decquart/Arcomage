a84baa3084bf00c086e7cf82a796f7de1d3ac7dc
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-08 23:06:33 +0300

    created new project based on razor pages, added changes

---
+++ Web/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,39 @@
+using BLL.DTO;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        private readonly IUserService _userService;
+
+        [BindProperty]
+        public LogInModel LogModel { get; set; }
+
+        public LoginModel(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new UserDtoForRegister{ Email = LogModel.Email, Password = LogModel.Password };
+                await _userService.Login(user);                
+            }
+            return Page();
+        }
+    }
+}
