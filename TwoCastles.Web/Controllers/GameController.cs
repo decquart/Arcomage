d6e6fbbce476ec7c733d02552ff0c6a1c7cc05a5
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-08 13:26:52 +0300

    create controllers, add methods

--- TwoCastles.Web/Controllers/GameController.cs
+++ TwoCastles.Web/Controllers/GameController.cs
@@ -3,8 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TwoCastles.Entities;
 using TwoCastles.GameLogic.Interfaces;
-using TwoCastles.GameLogic.Services;
 
 namespace TwoCastles.Web.Controllers
 {
@@ -15,18 +15,36 @@ namespace TwoCastles.Web.Controllers
         private readonly IGameService _gameService;
         private readonly IDeckService _deckService;
         private readonly ICardService _cardService;
+        private Game game;
 
         public GameController(IGameService gameService, ICardService cardService, IDeckService deckService)
         {
             _gameService = gameService;
             _deckService = deckService;
             _cardService = cardService;
+            game = _gameService.GetNewGame();
+
+        }
+        [HttpGet("play")]
+        public IActionResult Play()
+        {
+            _gameService.IncreasePlayerResource(game.FirstPlayer);
+            _deckService.Deal(game);
+            var firstPlayerCard = game.FirstPlayer.Hand.FirstOrDefault();
+            game.FirstPlayer.Hand.Remove(firstPlayerCard);
+            _deckService.PushCard(game, firstPlayerCard);
+            _cardService.Play(firstPlayerCard, game.FirstPlayer, game.SecondPlayer);
+
+
+            _gameService.IncreasePlayerResource(game.SecondPlayer);
+            var secondPlayerCard = game.SecondPlayer.Hand.FirstOrDefault();
+            _cardService.Play(secondPlayerCard, game.SecondPlayer, game.FirstPlayer);
+
+            return Ok();
         }
 
-        public void Play()
-        {            
-           
-        }
 
     }
+
 }
+
