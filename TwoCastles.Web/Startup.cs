fe8f83682e0a0c2feb4ef10596532158a14af0b6
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-08 13:28:27 +0300

    register services

--- TwoCastles.Web/Startup.cs
+++ TwoCastles.Web/Startup.cs
@@ -4,8 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using TwoCastles.Data.Interfaces;
+using TwoCastles.Data.Repositories;
 using TwoCastles.GameLogic.Interfaces;
 using TwoCastles.GameLogic.Services;
 
@@ -13,13 +19,22 @@ namespace TwoCastles.Web
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<IGameService, GameService>();
             services.AddTransient<ICardService, CardService>();
             services.AddTransient<IDeckService, DeckService>();
-            services.AddTransient<IGameService, GameService>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,6 +43,10 @@ namespace TwoCastles.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
