23effae18fbe182b00f137e16b775b6aa2dfe8a9
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-09 13:09:28 +0300

    added sing up model

---
+++ Web/Models/SignupModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    public class SignupModel
+    {
+        [Required(ErrorMessage = "Invalid Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Invalid Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
