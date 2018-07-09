796947970b8a1948b3cc17e71e029a3c386f6cd0
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-09 14:46:49 +0300

    created new razor pages

---
+++ Web/Pages/Game/List.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.DTO;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Game
+{
+    public class ListModel : PageModel
+    {
+        [BindProperty]
+        public List<GameDto> Games { get; set; }
+
+        private readonly IGameService _gameService;
+
+        public ListModel(IGameService gameService)
+        {
+            _gameService = gameService;
+        }        
+
+        public void OnGet()
+        {
+            Games = _gameService.GetGameList().ToList();
+        }
+    }
+}
---
+++ Web/Pages/Score/TopByGame.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.DTO;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Score
+{
+    public class TopByGameModel : PageModel
+    {
+        private readonly IScoreService _scoreService;
+
+        [BindProperty]
+        public List<ScoreDtoWithEmail> Scores { get; set; }
+
+        public TopByGameModel(IScoreService scoreService)
+        {
+            _scoreService = scoreService;
+        }
+
+        public void OnGet(int id)
+        {
+            Scores = _scoreService.GetScoresByGame(id).ToList();
+        }
+    }
+}
