900ddbea14742c3292ba4da9e1febd7b50fc0151
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-10 12:28:42 +0300

    replace solution into one folder

---
+++ Arcomage/Web/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.DTO;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web.Models;
+
+namespace Web.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly IUserService _userService;
+
+        [BindProperty]
+        public SignupModel SignupModel { get; set; }
+
+        public RegisterModel(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var usr = new UserDtoForRegister { Email = SignupModel.Email, Password = SignupModel.Password };
+                var result = await _userService.Register(usr);
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
+        }
+    }
+}
