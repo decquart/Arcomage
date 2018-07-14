943e675b01106839474a21b42c5845c753fb6939
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-14 18:14:59 +0300

    added  card list component

---
+++ AngularApp/src/app/card/card-list.component.ts
@@ -0,0 +1,36 @@
+import { Card } from "../models/card";
+import { Component } from '@angular/core';
+import { CardService } from "./card.service";
+
+@Component({
+    selector: 'my-card-list',
+    templateUrl: './card-list.component.html',
+    styleUrls: ['./card-list.component.css']
+})
+
+export class CardListComponent{
+    cards: Card[];
+    selectedCard: Card;
+    errorMessage: string;
+
+    constructor(private cardService: CardService){
+    }
+
+    select(card: Card){
+        this.selectedCard = card;
+    }
+
+    useCard(card: Card){
+        this.cardService.applyCard(card.name).subscribe(
+            selCard => this.selectedCard = selCard,
+            error => this.errorMessage = <any>error
+        )
+    };
+
+    ngOnInit(): void{
+        this.cardService.getCards().subscribe(
+            cards => this.cards = cards,
+            error => this.errorMessage = <any>error
+        );
+    }
+}
