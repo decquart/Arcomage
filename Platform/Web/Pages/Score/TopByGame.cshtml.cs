7c7c789f733e41db5c7ec8cccb5b1defcc09b684
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-15 14:38:36 +0300

    added game name to header

--- Platform/Web/Pages/Score/TopByGame.cshtml.cs
+++ Platform/Web/Pages/Score/TopByGame.cshtml.cs
@@ -14,18 +14,23 @@ namespace Web.Pages.Score
     public class TopByGameModel : PageModel
     {
         private readonly IScoreService _scoreService;
+        private readonly IGameService _gameService;
 
         [BindProperty]
         public List<ScoreDtoWithEmail> Scores { get; set; }
+        [BindProperty]
+        public GameDto Game { get; set; }
 
-        public TopByGameModel(IScoreService scoreService)
+        public TopByGameModel(IScoreService scoreService, IGameService gameService)
         {
             _scoreService = scoreService;
+            _gameService = gameService;
         }
 
         public void OnGet(int id)
         {
             Scores = _scoreService.GetScoresByGame(id).ToList();
+            Game = _gameService.Get(id);
         }
     }
 }
