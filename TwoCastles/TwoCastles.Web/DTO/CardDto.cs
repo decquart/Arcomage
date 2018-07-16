87dc63abe6059500b10df10045489e6e43106ce9
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-16 23:14:34 +0300

    created dto for   card

---
+++ TwoCastles/TwoCastles.Web/DTO/CardDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwoCastles.Web.DTO
+{
+    public class CardDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+    }
+}
