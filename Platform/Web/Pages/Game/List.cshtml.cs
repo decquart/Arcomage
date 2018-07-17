b31f21676e9fd8f5f66c37fb67fd478b19b2ff74
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-17 21:25:06 +0300

    added new property, inject service

--- Platform/Web/Pages/Game/List.cshtml.cs
+++ Platform/Web/Pages/Game/List.cshtml.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BLL.DTO;
 using BLL.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Web.Pages.Game
 {
@@ -15,17 +13,23 @@ namespace Web.Pages.Game
     {
         [BindProperty]
         public List<GameDto> Games { get; set; }
+        [BindProperty]
+        public string CurrentUserId { get; set; }
+
 
         private readonly IGameService _gameService;
+        private readonly IUserService _userService;
 
-        public ListModel(IGameService gameService)
+        public ListModel(IGameService gameService, IUserService userService)
         {
             _gameService = gameService;
+            _userService = userService;
         }        
 
         public void OnGet()
         {
             Games = _gameService.GetGameList().ToList();
+            CurrentUserId = _userService.GetUserId(User);
         }
     }
 }
