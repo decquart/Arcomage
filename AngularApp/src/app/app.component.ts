a4f803561b1b34383c35b218c9fecf8cd59c9d17
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-19 16:54:33 +0300

    found solution to get an id from url and set it to local storage

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'root',
   templateUrl: './app.component.html' , 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Arcomage';
+  private id: string;
+
+  constructor(private route: ActivatedRoute){ 
+    this.id = location.pathname.slice(1,location.pathname.length);
+    localStorage.setItem('id', this.id);
+    console.log(localStorage.getItem('id'));
+  }
+
+  ngOnInit(): void{
+  
+  }  
+ 
 }
