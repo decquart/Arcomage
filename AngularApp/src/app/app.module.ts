cb12f6b79cd3d2aba5b43bab57fdba8d17584aeb
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-19 16:46:39 +0300

    configure routing

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -7,13 +7,18 @@ import { CardService } from './card/card.service';
 import {HttpClientModule} from '@angular/common/http';
 import { CastleComponent } from './castle/castle.component';
 import { CastleService } from './castle/castle.service';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
       CardListComponent, AppComponent, CastleComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule,
+    RouterModule.forRoot([
+      {path: ':id', component: AppComponent},
+      {path: '**', redirectTo:'', pathMatch: 'full'}
+    ])
   ],
   providers: [CardService, CastleService],
   bootstrap: [AppComponent]
