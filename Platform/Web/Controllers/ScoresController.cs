0805892726475f3182f8274b7e1aabb0fc3fdb8c
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-20 13:20:57 +0300

    created controller and model

---
+++ Platform/Web/Controllers/ScoresController.cs
@@ -0,0 +1,40 @@
+﻿using BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class ScoresController : Controller
+    {
+        private readonly IScoreService _scoreService;
+
+        public ScoresController(IScoreService scoreService)
+        {
+            _scoreService = scoreService;
+        }
+
+
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create([FromBody] ScoreModel scoreModel)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest();
+
+                _scoreService.AddScore(scoreModel.UserId, scoreModel.GameId, scoreModel.Value);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+    }
+}
