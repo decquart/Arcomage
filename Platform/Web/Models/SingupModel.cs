c977b9b8de54ff982ef8916a6d1a6210a0302926
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-21 22:35:31 +0300

    fix view and validation

--- Platform/Web/Models/SingupModel.cs
+++ Platform/Web/Models/SingupModel.cs
@@ -4,7 +4,8 @@ namespace Web.Models
 {
     public class SignupModel
     {
-        [Required(ErrorMessage = "Invalid Email")]
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Invalid Password")]
