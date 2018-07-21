48eb42fd137f1fbdc9c2a5502cf2fce9af147032
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-21 11:18:59 +0300

    added api service

---
+++ TwoCastles/TwoCastles.GameLogic/Interfaces/IApiService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoCastles.GameLogic.Interfaces
+{
+    public interface IApiService
+    {
+        void Post<T>(string url, T model);
+    }
+}
---
+++ TwoCastles/TwoCastles.GameLogic/Services/ApiService.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp;
+using TwoCastles.GameLogic.Interfaces;
+
+namespace TwoCastles.GameLogic.Services
+{
+    public class ApiService : IApiService
+    {
+        public void Get<T>(string url)
+        {
+
+        }
+
+        public void Post<T>(string url, T model)
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.POST);
+
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(model);
+            IRestResponse response = client.Execute(request);
+        }
+    }
+}
