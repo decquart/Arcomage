94c350320934085cd8d41b725e7a8607b860ca33
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-23 23:47:51 +0300

    created new facade service

---
+++ TwoCastles/TwoCastles.GameLogic/Interfaces/IGamePipelineService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwoCastles.Entities;
+
+namespace TwoCastles.GameLogic.Interfaces
+{
+    public interface IGamePipelineService
+    {
+        string PlayerTurn(Game game, Card playerCard, Player currentPlayer, Player enemyPlayer);
+        string ComputerTurn(Game game, Card computerPlayerCard, Player computerPlayer, Player humanPlayer);
+    }
+}
---
+++ TwoCastles/TwoCastles.GameLogic/Services/GamePipelineService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwoCastles.Entities;
+using TwoCastles.GameLogic.Interfaces;
+
+namespace TwoCastles.GameLogic.Services
+{
+    public class GamePipelineService : IGamePipelineService
+    {
+        private readonly IGameService _gameService;
+        private readonly IDeckService _deckService;
+        private readonly ICardService _cardService;
+
+        public GamePipelineService(IGameService gameService, IDeckService deckService,
+                                    ICardService cardService)
+        {
+            _gameService = gameService;
+            _deckService = deckService;
+            _cardService = cardService;
+        }
+
+        public string PlayerTurn(Game game, Card playerCard, Player currentPlayer, Player enemyPlayer)
+        {
+            var isEnoughRes = _cardService.IsEnoughResources(playerCard, game.FirstPlayer);
+            if (!isEnoughRes)
+                throw new ApplicationException($"Player doesn't have enough resources to apply {playerCard.Name}");
+
+            _gameService.IncreasePlayerResource(currentPlayer);
+            currentPlayer.Hand.Remove(playerCard);
+            _deckService.PushCard(game, playerCard);
+            _cardService.Play(playerCard, currentPlayer, enemyPlayer);
+            _deckService.GiveCardToPlayer(game, currentPlayer);
+            _gameService.NormalizeCastles(game);
+            _gameService.IncreasePlayerScore(currentPlayer, playerCard);
+            return _gameService.CheckWinner(game);      //return winner id
+        }
+
+        public string ComputerTurn(Game game, Card computerPlayerCard, Player computerPlayer, Player humanPlayer)
+        {
+            _gameService.IncreasePlayerResource(computerPlayer);
+            computerPlayer.Hand.Remove(computerPlayerCard);
+            _deckService.PushCard(game, computerPlayerCard);
+            var isEnoughRes = _cardService.IsEnoughResources(computerPlayerCard, computerPlayer);
+            //apply card if enough resources
+            if (isEnoughRes)
+                _cardService.Play(computerPlayerCard, computerPlayer, humanPlayer);
+
+            _deckService.GiveCardToPlayer(game, computerPlayer);
+            _gameService.NormalizeCastles(game);
+            _gameService.IncreasePlayerScore(computerPlayer, computerPlayerCard);
+            _gameService.CheckWinner(game);
+            return _gameService.CheckWinner(game);
+        }
+    }
+}
