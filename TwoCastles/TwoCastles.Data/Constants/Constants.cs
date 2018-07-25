67f0ac54921c45eb174c8a0d224f101dd247a187
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-25 18:22:46 +0300

    created class which'll contain constants

---
+++ TwoCastles/TwoCastles.Data/Constants/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoCastles.Data.Constants
+{
+    public static class Constants
+    {
+        public const int maxPlayerCards = 6;
+    }
+}
