f3811388965a47847e830d9fc14f9f09ebbfc34a
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-26 21:23:52 +0300

    displacement services into separate folder, start to use http service

---
+++ AngularApp/src/app/services/card.service.ts
@@ -0,0 +1,30 @@
+import { Card } from '../models/card';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpService } from '../services/http.services';
+
+@Injectable()
+
+export class CardService{
+    private cardUrl = 'https://localhost:44347/api/game/';
+
+    constructor(private httpService: HttpService){
+
+    }
+    getCards(): Observable<Card[]>{ 
+      return this.httpService.get(this.cardUrl + 'cards/' +
+       localStorage.getItem('id'));             
+    }
+
+    applyCard(cardName: string): Observable<any>{
+      return this.httpService.get(this.cardUrl + 'play/' + cardName + '/' +
+        localStorage.getItem('id')
+        );             
+    }
+
+    discard(cardName: string): Observable<any>{
+      return this.httpService.get(this.cardUrl + 'discard/' + cardName + '/' +
+      localStorage.getItem('id')
+        );             
+    }
+}
---
+++ AngularApp/src/app/services/castle.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpService } from '../services/http.services';
+import { Castle } from '../models/castle';
+
+@Injectable()
+
+export class CastleService{
+    private castleUrl = 'https://localhost:44347/api/game/castles/';
+    constructor(private httpService: HttpService){
+
+    }
+
+    getCastles(): Observable<Castle[]>{
+        return this.httpService.get(this.castleUrl + localStorage.getItem('id')
+          );             
+      }   
+}
