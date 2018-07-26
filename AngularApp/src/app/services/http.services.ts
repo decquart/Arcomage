dbe4809f9d40bc40fb5d3808d07b03495fcc912c
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-26 21:22:41 +0300

    created http service

---
+++ AngularApp/src/app/services/http.services.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { catchError } from 'rxjs/operators/catchError';
+import { tap } from 'rxjs/operators/tap';
+
+@Injectable()
+
+export class HttpService{
+
+    constructor(private http: HttpClient){}
+
+    get(url: string): Observable<any>{
+        return this.http.get<any>(url).
+        pipe(tap(data => console.log('All: ' + JSON.stringify(data))),
+            catchError(this.handleError)
+          );             
+      }
+
+
+
+    private handleError(err: HttpErrorResponse) {
+        // in a real world app, we may send the server to some remote logging infrastructure
+        // instead of just logging it to the console
+        let errorMessage = ``;
+        if (err.error instanceof Error) {
+          // A client-side or network error occurred. Handle it accordingly.
+          errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+          // The backend returned an unsuccessful response code.
+          // The response body may contain clues as to what went wrong,
+          errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return Observable.throw(errorMessage);
+      }
+}
