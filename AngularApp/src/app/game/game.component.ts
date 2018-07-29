7a75f61c34f2eb239443bda0915f3e7e0e5d9945
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-29 21:19:50 +0300

    changed game component

--- AngularApp/src/app/game/game.component.ts
+++ AngularApp/src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GameService } from '../services/game.service';
 
 @Component({
     selector: 'game',
@@ -10,13 +11,10 @@ import { ActivatedRoute } from '@angular/router';
 export class GameComponent implements OnInit{
     private id: string;
 
-    constructor(private route: ActivatedRoute){
-        
-    }
+    constructor(private route: ActivatedRoute, private gameService: GameService){}
 
     ngOnInit(): void{
         this.id = location.pathname.slice(6,location.pathname.length);
         localStorage.setItem('id', this.id);
-        console.log(localStorage.getItem('id'));
       }
 }
