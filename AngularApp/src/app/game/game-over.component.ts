93695d97dcdc632efe118cae6c18b2acc1ffe787
Volodymyr Mikhaylyuk <decquart@gmail.com>
2018-07-29 21:18:02 +0300

    changed "game over" component, added styles

--- AngularApp/src/app/game/game-over.component.ts
+++ AngularApp/src/app/game/game-over.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'game-over',
     templateUrl: './game-over.component.html',
+    styleUrls: ['./game-over.component.css']
 })
 
 export class GameOverComponent{
